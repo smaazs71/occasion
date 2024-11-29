0f1a0ebfe17dba42863795195bd3afbd24b06fcb
maazcloudhire <167092294+maazcloudhire@users.noreply.github.com>
2024-11-29 19:54:26 +0530

    7th commit

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import InvitationCard from "./components/InvitationCard";
 import Advertisement from "./components/Advertisement";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
+import Head from "next/head";
 
 function InvitationContent() {
   const searchParams = useSearchParams();
@@ -34,8 +35,26 @@ function InvitationContent() {
 
 export default function Page() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <InvitationContent />
-    </Suspense>
+    <>
+      <Head>
+        <title>Wedding Invitation</title>
+        <meta property="og:title" content="You're Invited!" />
+        <meta
+          property="og:description"
+          content="Join us for the joyous union of [Bride's Name] & [Groom's Name]. Your presence will make our day extra special!"
+        />
+        <meta property="og:image" content="public\images\ogimage.webp" />
+        <meta
+          property="og:url"
+          content="https://occasion.vercel.app/?id=1&name=maaz"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Wedding Invitation" />
+      </Head>
+      {/* Page Content */}
+      <Suspense fallback={<p>Loading...</p>}>
+        <InvitationContent />
+      </Suspense>
+    </>
   );
 }
